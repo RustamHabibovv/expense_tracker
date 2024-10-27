098e38fdd1b5126169de4b5f3dea30179f301f82
RustamHabibovv <rustamhabibov2004@gmail.com>
2024-10-27 13:43:23 +0200

    Transactions list view completed

--- lib/pages/budget.dart
+++ lib/pages/budget.dart
@@ -1,90 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:expense_tracker/data/data.dart';
+import 'package:expense_tracker/pages/stats.dart';
 
-
-class BudgetPage extends StatelessWidget {
+class BudgetPage extends StatefulWidget {
   const BudgetPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return  Scaffold(
+  State<BudgetPage> createState() => _BudgetPageState();
+}
 
+class _BudgetPageState extends State<BudgetPage> {
+  int _currentIndex = 0; // To track the current index
+
+  // List of pages for navigation
+  final List<Widget> _pages = [
+    BudgetScreen(),
+    StatsScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
         elevation: 0,
         shadowColor: Colors.black,
         toolbarHeight: 40,
         leading: const Icon(Icons.wallet),
         titleSpacing: 0,
-        title: const Text('Budget',style: TextStyle(fontWeight: FontWeight.bold),),
-        
-        
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            Container(
-              width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.height/3-30,
-              decoration:  BoxDecoration(
-                color: Colors.lightBlue,
-                borderRadius: BorderRadius.circular(25),
-              ),
-              child: const Column(  
-                children: [
-                  SizedBox(height: 16,),
-                  Text('Total Balance',style: TextStyle(color: Colors.white, fontWeight: FontWeight.w600, fontSize: 20),),
-                  
-                  Text('\$ 4500.00', style: TextStyle(color: Colors.white, fontWeight: FontWeight.w600, fontSize: 40),),
-            
-                  Padding(
-                    padding: EdgeInsets.only(top: 20, left: 16),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    
-                      children: [
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            Text('Income',style: TextStyle(color: Colors.white, fontWeight: FontWeight.w500, fontSize: 16),),
-                            Text('\$ 4000.00',style:TextStyle(color: Colors.white, fontWeight: FontWeight.w500, fontSize: 20))
-                          ],
-                        ),
-            
-                        
-                        Padding(
-                          padding: EdgeInsets.only(right: 16),
-                          child: Column(
-                            
-                            children: [
-                              Text('Expenses',style: TextStyle(color: Colors.white, fontWeight: FontWeight.w500, fontSize: 16),),
-                              Text('\$ 1500.00',style:TextStyle(color: Colors.white, fontWeight: FontWeight.w500, fontSize: 20))
-                            ],
-                          ),
-                        )
-            
-                      ],
-                    ),
-                  )
-                  
-                ],  
-              ),
-            ),
-            const SizedBox(height: 40,),
-            const Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text('Transactions',style: TextStyle(fontSize: 16,fontWeight: FontWeight.w600),),
-                Text('View All',style: TextStyle(fontSize: 14),)
-              ],
-            )
-          ],
+        title: const Text(
+          'Budget',
+          style: TextStyle(fontWeight: FontWeight.bold),
         ),
-        
       ),
-      
+      body: _pages[_currentIndex], // Display the current page
 
       bottomNavigationBar: BottomNavigationBar(
-      items: const <BottomNavigationBarItem>[
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
             icon: Icon(Icons.home),
             label: 'Home',
@@ -100,11 +57,178 @@ class BudgetPage extends StatelessWidget {
         onPressed: () {},
         shape: const CircleBorder(),
         child: const Icon(Icons.add),
- 
       ),
+    );
+  }
+}
 
-
-
+class BudgetScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height / 3 - 30,
+            decoration: BoxDecoration(
+              color: Colors.lightBlue,
+              borderRadius: BorderRadius.circular(25),
+            ),
+            child: const Column(
+              children: [
+                SizedBox(height: 16),
+                Text(
+                  'Total Balance',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w600,
+                      fontSize: 20),
+                ),
+                Text(
+                  '\$ 4500.00',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w600,
+                      fontSize: 40),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 20, left: 16),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Income',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.w500,
+                                fontSize: 16),
+                          ),
+                          Text('\$ 4000.00',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 20))
+                        ],
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(right: 16),
+                        child: Column(
+                          children: [
+                            Text(
+                              'Expenses',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 16),
+                            ),
+                            Text('\$ 1500.00',
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w500,
+                                    fontSize: 20))
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+          const SizedBox(height: 40),
+          const Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'Transactions',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+              ),
+              Text(
+                'View All',
+                style: TextStyle(fontSize: 14),
+              )
+            ],
+          ),
+          const SizedBox(height: 16),
+          Expanded(
+            child: ListView.builder(
+              shrinkWrap: true,
+              padding: const EdgeInsets.all(8),
+              itemCount: transactionsData.length,
+              itemBuilder: (context, int i) {
+                return Padding(
+                  padding: const EdgeInsets.only(bottom: 16),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Stack(
+                                alignment: Alignment.center,
+                                children: [
+                                  Container(
+                                    width: 50,
+                                    height: 50,
+                                    decoration: BoxDecoration(
+                                      color: transactionsData[i]['color'],
+                                      shape: BoxShape.circle,
+                                    ),
+                                  ),
+                                  transactionsData[i]['icon'],
+                                ],
+                              ),
+                            ),
+                            const SizedBox(width: 8),
+                            Text(
+                              transactionsData[i]['name'],
+                              style: const TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(right: 8),
+                          child: Column(
+                            children: [
+                              Text(
+                                transactionsData[i]['totalAmount'],
+                                style: const TextStyle(
+                                    fontSize: 14, fontWeight: FontWeight.w400),
+                              ),
+                              Text(
+                                transactionsData[i]['date'],
+                                style: const TextStyle(
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w400,
+                                  color: Colors.grey,
+                                ),
+                              )
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
